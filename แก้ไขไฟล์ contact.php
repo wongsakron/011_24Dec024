23a24da9ad36ff7347f27ad65e222de5593b9957
wongsakron <skizz3848@gmail.com>
2024-12-26 15:50:40 +0700

    zcvzxv

--- แก้ไขไฟล์ contact.php
+++ แก้ไขไฟล์ contact.php
@@ -2,12 +2,12 @@
     <table width="1238" height="530" border="0">
         <tr>
             <td width="23" rowspan="2">&nbsp;</td>
-            <td height="119 colspan="2" bgcolor="#006600">
-                <h1><center><font color="#FFFFFF">พรชัยอพาร์ตเมนต์</font></center></h1>
+            <td height="119 colspa2" bgcolor="#006600">
+                <h1>
                 
             </td>
             <td width="43" rowspan="2">&nbsp;</td>
-        </tr>
+        </tr>sdfdsf
         <tr>
             <td width="1007" bgcolor="#FFFFFF">
                 <img src="images/map.gif">
